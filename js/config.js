a2e2c9639338d6ffc4e2e880dbec670fc27fcb8d
Angel <angelgct05@hotmail.com>
2026-02-24 15:47:34 -0600

    Configuracion en supabase

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-export const SUPABASE_URL = "https://hazodxcwcxeripkbnzjy.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhhem9keGN3Y3hlcmlwa2Juemp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE1MDkzNzcsImV4cCI6MjA4NzA4NTM3N30.EU2twiW4ywXUjJnMFdtDy_cKqYgGTZVMURquUOh_Lt8";
+export const SUPABASE_URL = "https://fhybflxdshortjwjejmp.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoeWJmbHhkc2hvcnRqd2plam1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NjQ5MjIsImV4cCI6MjA4NzU0MDkyMn0.PX_PwO2WUQep_KMp8N8sefD_gjifpqekP4AjqebV6Nw";
 export const ROOM_CODE = "SALA1";
